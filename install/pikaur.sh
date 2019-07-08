c76b95b80047173dfae3e0abf8710c37b718aaaf
Felix Münscher <Felix.Muenscher@mni.thm.de>
2019-07-08 19:59:20 +0200

    Updates Picaur Script

--- install/pikaur.sh
+++ install/pikaur.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ $(/usr/bin/id -u) -ne 0 ]; then
-    echo "Not running with sudo"
-    exit
-fi
-
 sudo pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/pikaur.git
 
