455d2f1c56ef0849aedbb4fa9dab76795e46e4d5
Felix Münscher <Felix.Muenscher@mni.thm.de>
2019-10-09 14:26:20 +0200

    Moves GitConfig generation into seperate Script

---
+++ bin/generate_gitconfig.sh
@@ -0,0 +1,7 @@
+read -p 'Full Name: ' uservar
+read -p 'E-Mail Adress: ' mailvar
+
+read -p "Correct? Press [Enter] key to continue..."
+
+sed "s/<USER>/$uservar/g" ../gitconfig_blueprint > ../gitconfig
+sed -i "s/<MAIL>/$mailvar/g" ../gitconfig
--- setup_debian.sh
+++ setup_debian.sh
@@ -1,12 +1,9 @@
 current=$(pwd)
 
-read -p 'Full Name: ' uservar
-read -p 'E-Mail Adress: ' mailvar
+echo "Setup-Script Started"
 
-read -p "Correct? Press [Enter] key to continue..."
-
-sed "s/<USER>/$uservar/g" ./gitconfig_blueprint > ./gitconfig
-sed -i "s/<MAIL>/$mailvar/g" ./gitconfig
+echo "Generating GitConfig"
+sh bin/generate_gitconfig.sh
 
 echo "Setting up: zshrc"
 ln -sf $current/zshrc ~/.zshrc
