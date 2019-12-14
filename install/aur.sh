d4af84f3132c9484bb62b0b08242a38d6568648a
Felix Münscher <Felix.Muenscher@mni.thm.de>
2019-12-14 20:31:33 +0100

    Stabilizes AUR

--- install/aur.sh
+++ install/aur.sh
@@ -19,5 +19,9 @@ pikaur -Sy
 
 for package in ${aur_packages[@]};
 do
-  pikaur -S --noconfirm $package
+  if which pikaur &> /dev/null; then
+      sudo pikaur -S --noconfirm $package
+  else
+      echo "Pikaur is not installed! Please install it first"
+  fi
 done;
